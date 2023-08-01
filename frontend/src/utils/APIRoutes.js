d80ebe055c0a6a0fdba1cf91531f55cbc7b8ae6c
flybear5500 <flybear5500@gmail.com>
2023-08-01 22:33:44 +0000

    deploy init

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 
-export const host = "http://localhost:5000";
+export const host = process.env.REACT_APP_BACKEND_URL;
 export const calcFine = `${host}/api/search/keyword/calcFine`;
 export const searchAddress = `${host}/api/search/keyword/getAddress`;
 export const calcAllAddress = `${host}/api/search/keyword/calcAllAddress`;
